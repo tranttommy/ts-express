aaae3b6c683b84628836dfda4c1af638d1ae006b
Tommy Tran <tommy@squishymedia.com>
2021-02-04 13:28:24 -0800

    minor clean up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express'
-import router from './routes'
+import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.use('/', router)
+app.use('/', routes)
 
 app.listen(5000, () =>  console.log('Server at localhost:5000'))
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction} from 'express'
+import { Router, Request, Response } from 'express'
 
 export default Router()
   .get('/hello', (req: Request, res: Response) => res.send({ hello: 'world'}))
